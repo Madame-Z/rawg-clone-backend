6390393b9a746d3b7726c60cb564adde84ad12bb
Madame-Z <madame-z@protonmail.ch>
2022-03-11 16:38:30 +0100

    fix

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import { router as routeRegistration } from './routes/registration.js';
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 const MONGODB_URL = process.env.MONGODB_URL;
 //Connection DB 
 app.use(routeRegistration)
 mongoose.connect(`mongodb://${MONGODB_URL}/rawg`, () =>
-    console.log('Connected to DB' + MONGODB_URL));
+    console.log('Connected to DB'));
     
 const PORT = process.env.PORT;
 //Listening to the server
